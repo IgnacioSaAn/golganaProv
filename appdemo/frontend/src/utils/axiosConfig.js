e64d1000bd361b88374cdc9f54fcd3ce4ecb532d
IgnacioSaAn <ignaciosanchezagnello@gmail.com>
2025-06-17 18:23:31 -0300

    app funcional

---
+++ appdemo/frontend/src/utils/axiosConfig.js
@@ -0,0 +1,7 @@
+// utils/axiosConfig.js
+import axios from "axios";
+
+// Aquí puedes agregar cualquier configuración global de Axios
+axios.defaults.baseURL = "https://golgana.com"; // Ajusta el URL base de tu API
+
+export default axios;
